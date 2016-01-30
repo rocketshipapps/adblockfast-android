304bc9b3fd3f22cef081b131f51fb50c244dd28c
Norman Paniagua <normanpaniagua@gmail.com>
2016-01-30 17:46:48 -0300

    Added OS field to device registration

--- app/src/main/java/com/rocketshipapps/adblockfast/service/RegistrationIntentService.java
+++ app/src/main/java/com/rocketshipapps/adblockfast/service/RegistrationIntentService.java
@@ -52,10 +52,11 @@ public class RegistrationIntentService extends IntentService {
     }
 
     private void sendRegistrationTokenToServer(String token) {
-        if (sharedPreferences.getBoolean(Preferences.SENT_TOKEN_TO_SERVER, false)) return;
+        //if (sharedPreferences.getBoolean(Preferences.SENT_TOKEN_TO_SERVER, false)) return;
 
         List<Pair<String,String>> params = new ArrayList<>();
         params.add(new Pair<>("token", token));
+        params.add(new Pair<>("os", "android"));
 
         Pair<String, String> header = new Pair<>("x-application-secret", Preferences.SECRET);
 
