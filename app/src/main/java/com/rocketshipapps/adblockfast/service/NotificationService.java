f2132816c6ff35150a4ba44b24ecebb80d2be0be
Norman Paniagua <normanpaniagua@gmail.com>
2016-01-30 15:26:28 -0300

    Updated push notification icon

--- app/src/main/java/com/rocketshipapps/adblockfast/service/NotificationService.java
+++ app/src/main/java/com/rocketshipapps/adblockfast/service/NotificationService.java
@@ -31,7 +31,7 @@ public class NotificationService extends GcmListenerService {
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.mipmap.ic_launcher)
+                .setSmallIcon(R.drawable.notification)
                 .setContentTitle(title)
                 .setContentText(message)
                 .setAutoCancel(true)
